58e70152c3698a537795f558c3f09bdde5729c7b
peter david <princewill0162gmail.com>
2025-04-14 18:58:42 +0000

    done

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+  for (const department in report.allEmployees) {
+    employees.push(...report.allEmployees[department]);
+  }
+  
+  return employees;
+}
